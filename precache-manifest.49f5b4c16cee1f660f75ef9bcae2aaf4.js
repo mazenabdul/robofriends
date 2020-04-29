1abfd743d2850a0c744cd70fa7c1bebd72af9c68
mazenabdul <63692059+mazenabdul@users.noreply.github.com>
2020-04-29 19:38:32 -0400

    Updates

---
+++ precache-manifest.49f5b4c16cee1f660f75ef9bcae2aaf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf196954ffd6fcbade0393517f8b30b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d05b5f68b95717779513",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "12db07f483361434592c",
+    "url": "/robofriends/static/css/main.c8452517.chunk.css"
+  },
+  {
+    "revision": "d05b5f68b95717779513",
+    "url": "/robofriends/static/js/2.507a2d19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.507a2d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12db07f483361434592c",
+    "url": "/robofriends/static/js/main.4a781a34.chunk.js"
+  },
+  {
+    "revision": "3a6cd7bf0ee3b3b38bcf",
+    "url": "/robofriends/static/js/runtime-main.db3a75f1.js"
+  }
+]);
